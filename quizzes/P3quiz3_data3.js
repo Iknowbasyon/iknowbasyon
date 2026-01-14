b0c231050b732c739f26b8fe7c2bcaa4d9c9b63d
Dexx <ragnarokeneftee@gmail.com>
2026-01-14 21:55:51 +0800

    Update P3quiz3_data3.js

--- quizzes/P3quiz3_data3.js
+++ quizzes/P3quiz3_data3.js
@@ -27,7 +27,7 @@ document. addEventListener('DOMContentLoaded', function() {
 
   // Global Variables
   let currentUser = null;
-  let timer = 2 * 60;
+  let timer = 4 * 60;
   let timerInterval = null;
   let timeUp = false;
   let currentQuestionIndex = 0;
@@ -146,7 +146,7 @@ async function fetchQuestionsFromSupabase() {
 
   function startTimer() {
     console.log('⏱️ Timer started');
-    timer = 2 * 60;
+    timer = 4 * 60;
     timeUp = false;
     updateTimer();
     
